8831e9a93e7e8e3b0a31616e9d50cb1274547dcb
ShawnDietrich <75051081+ShawnDietrich@users.noreply.github.com>
2022-03-09 19:47:51 -0500

    Changed App function to a compoent

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,19 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div>
-    <h1>Ja<span className="highlight">mmm</span>ing</h1>
-    <div className="App">
-      <SearchBar />
-      <div className="App-playlist">
-        <SearchResults />
-        <Playlist />
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults />
+            <Playlist />
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-  );
+    );
+  }
 }
 
 export default App;
